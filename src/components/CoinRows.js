b8ddec07e04b5e25acc92fb921482975aa7ff61d
Marcos-Caballero <robayo1100@gmail.com>
2023-09-23 17:41:05 -0500

    Ajustando la tabla y estilizando un poco

--- src/components/CoinRows.js
+++ src/components/CoinRows.js
@@ -4,7 +4,17 @@ const CoinRow = ({coin, index}) => {
     console.log(coin, index)
     return (
         <tr key={coin.name}>
-            <td>{coin.name}</td>
+            <td>{index}</td>
+            <td>
+                <img src={coin.image} alt={coin.name} style={{width: '10%'}} className='me-4 imf-fluid'></img>
+                <span>{coin.name}</span>
+                <span className='ms-3 text-muted text-uppercase'>{coin.symbol}</span>
+                </td>
+            <td>{coin.current_price}</td>
+            <td className={coin.price_change_percentage_24h > 0 ? 'text-success' : 'text-danger'}>
+                {coin.price_change_percentage_24h}
+            </td>
+            <td>{coin.total_volume}</td>
         </tr>
     )
 }
--- src/components/TableCoins.js
+++ src/components/TableCoins.js
@@ -1,17 +1,25 @@
 import React from "react";
 import CoinRow from "./CoinRows";
 
+const headers = ['#', 'Moneda', 'Precio', 'Porcentaje de cambio', '24 movimientos']
+
 const TableCoins = ({coins}) => {
     return (
         <table className="table table-dark mt-4 table-hover">
             <thead>
                 <tr>
-                    <td>name</td>
+                    {
+                    headers.map((header) => (
+                        <td>{header}</td>
+                    ))
+                    }
+                </tr>
+                <tr>
                 </tr>
             </thead>
             <tbody>
                 {coins.map((coin, index) => (
-                    <CoinRow coin={coin} key={index} />
+                    <CoinRow coin={coin} key={index} index={index + 1} />
                 ))}
             </tbody>
         </table>
