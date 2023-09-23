743538cb5ea709743552a7e74ab10c3e142637b7
Marcos-Caballero <robayo1100@gmail.com>
2023-09-23 16:55:37 -0500

    Integrando la API y bootstrap

---
+++ src/components/TableCoins.js
@@ -0,0 +1,21 @@
+import React from "react";
+import CoinRow from "./CoinRows";
+
+const TableCoins = ({coins}) => {
+    return (
+        <table className="table table-dark mt-4 table-hover">
+            <thead>
+                <tr>
+                    <td>name</td>
+                </tr>
+            </thead>
+            <tbody>
+                {coins.map((coin, index) => (
+                    <CoinRow coin={coin} key={index} />
+                ))}
+            </tbody>
+        </table>
+    )
+}
+
+export default TableCoins
